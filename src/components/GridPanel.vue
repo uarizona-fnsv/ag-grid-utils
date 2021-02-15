ec603c019a050e7e3a07099ddc704c4db115bfb5
Brock McElroy <28877984+brxck@users.noreply.github.com>
2021-02-15 15:14:11 -0700

    Add grid panel component and story

---
+++ src/components/GridPanel.vue
@@ -0,0 +1,85 @@
+<template>
+  <div
+    ref="root"
+    class="w-100"
+    style="font-size: 1rem; font-family: var(--font-family-sans-serif);"
+  >
+    <div
+      class="card-header sticky-top shadow-sm mb-2 border-right d-flex justify-content-between align-items-center"
+    >
+      <!-- @slot Title display of the panel -->
+      <slot name="title">
+        <div></div>
+      </slot>
+      <div>
+        <!-- @slot Additional controls for header -->
+        <slot name="extra-controls" />
+        <!-- @slot Override panel controls entirely -->
+        <slot name="controls">
+          <button
+            aria-label="Resize panel"
+            class="btn btn-sm btn-outline-dark mr-1"
+            title="Resize panel"
+            @click="resizePanel"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <polyline points="18 8 22 12 18 16"></polyline>
+              <polyline points="6 8 2 12 6 16"></polyline>
+              <line x1="2" y1="12" x2="22" y2="12"></line>
+            </svg>
+          </button>
+          <button
+            class="btn btn-sm btn-outline-dark"
+            aria-label="Close panel"
+            title="Close panel"
+            @click="closePanel"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-x"
+            >
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg></button
+        ></slot>
+      </div>
+    </div>
+    <!-- @slot Body for the panel -->
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RelatedPanel",
+  methods: {
+    resizePanel() {
+      const el = this.$refs.root.closest(".ag-tool-panel-wrapper")
+      el.style.width = parseInt(el.style.width) > 550 ? "550px" : "800px"
+    },
+    closePanel() {
+      this.params.api.closeToolPanel()
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ stories/GridPanel.stories.js
@@ -0,0 +1,79 @@
+import Vue from "vue"
+import { AgGridVue } from "ag-grid-vue"
+import axios from "axios"
+
+import Datasource from "../src/utils/datasource"
+import GridPanel from "../src/components/GridPanel"
+import columnDefs from "./mocks/olympicColumnDefs"
+
+export default {
+  title: "Components/GridPanel",
+  component: GridPanel,
+}
+
+export const CustomPanel = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { GridPanel },
+  template: `
+      <GridPanel>
+        <template v-slot:title>
+          <h5 class="m-0">Custom</h5>
+        </template>
+        <template v-slot:controls>
+          <button class="btn btn-sm btn-primary mr-1">Reset</button>
+        </template>
+        <template>
+          <p class="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </template>
+      </GridPanel>`,
+})
+
+const GridPanelExample = Vue.extend(CustomPanel([], { argTypes: {} }))
+
+export const InGrid = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { AgGridVue, GridPanelExample },
+  template: `
+    <AgGridVue
+      class="ag-theme-balham"
+      :grid-options="gridOptions"
+      @grid-ready="onReady"
+      style="width: calc(100vw - 30px); height: calc(100vh - 3.5rem);"
+    />`,
+
+  methods: {
+    onReady() {
+      const gridApi = this.gridOptions.api
+      const ds = new Datasource({ gridApi, path: "/olympic/" })
+      gridApi.setServerSideDatasource(ds)
+    },
+    getOptions: async (field, value) => {
+      const res = await axios.get("/autocomplete/", {
+        params: { field, value, limit: 25 },
+      })
+      return res.data
+    },
+  },
+  data() {
+    return {
+      gridOptions: {
+        rowModelType: "serverSide",
+        frameworkComponents: { GridPanelExample: "GridPanelExample" },
+        sideBar: {
+          toolPanels: [
+            {
+              id: "customPanel",
+              labelDefault: "Custom Panel",
+              labelKey: "customPanel",
+              iconKey: "filter",
+              toolPanelFramework: "GridPanelExample",
+            },
+          ],
+          defaultToolPanel: "customPanel",
+        },
+        columnDefs,
+      },
+    }
+  },
+})
