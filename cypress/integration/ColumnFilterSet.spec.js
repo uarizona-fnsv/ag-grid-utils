37a8d07aba0c8fb79c2ef0879cfe98b025430fd8
Brock McElroy <28877984+brxck@users.noreply.github.com>
2021-02-23 13:30:11 -0700

    Add filterset test

---
+++ cypress/integration/ColumnFilterSet.spec.js
@@ -0,0 +1,53 @@
+context("Primary", () => {
+  before(() => {
+    cy.visitStorybook()
+  })
+
+  beforeEach(() => {
+    cy.loadStory("Filters/ColumnFilterSet", "Primary")
+  })
+
+  it("filters the grid", () => {
+    cy.get(".ag-header-cell[col-id=athlete] .ag-header-icon")
+      .first()
+      .as("menu")
+      .click()
+
+    cy.grab("cfs-unselected")
+      .as("unselected")
+      .first()
+      .click()
+    cy.grab("cfs-filter")
+      .as("filter")
+      .type("brooke")
+    cy.grab("cfs-unselected")
+      .should("contain", "Brooke")
+      .first()
+      .click()
+
+    cy.grab("cfs-selected")
+      .as("selected")
+      .should("contain", "Aleksey Nemov")
+      .should("contain", "Brooke Bennett")
+      .should("have.length", 2)
+    cy.get(".ag-header-viewport").click() // click outside
+
+    cy.get(".ag-center-cols-container .ag-row")
+      .find("[col-id=athlete]")
+      .as("rows")
+      .should("contain", "Aleksey Nemov")
+      .should("contain", "Brooke Bennett")
+      .should("have.length", 2)
+
+    cy.get("@menu").click()
+    cy.get("@filter").should("have.value", "")
+    cy.get("@selected")
+      .should("contain", "Aleksey Nemov")
+      .should("contain", "Brooke Bennett")
+      .should("have.length", 2)
+
+    cy.grab("cfs-clear").click()
+    cy.get(".ag-header-viewport").click() // click outside
+    cy.get("@rows").should("to.have.length.greaterThan", 2)
+  })
+})
--- src/components/ColumnFilterSet.vue
+++ src/components/ColumnFilterSet.vue
@@ -11,10 +11,17 @@
         type="text"
         placeholder="Filter options"
         class="form-control form-control-sm mt-1"
+        data-cy="cfs-filter"
       />
     </div>
     <div class="px-2" style="max-height: 400px; overflow-y: scroll;">
-      <div class="py-1" @click="toggleBlanks">
+      <div
+        v-if="
+          params.colDef.filterParams.blanks && !selected.includes('(Blanks)')
+        "
+        class="py-1"
+        @click="toggleBlanks"
+      >
         <input :checked="blanksFilter" type="checkbox" />
         <span class="pl-2">(Blanks)</span>
       </div>
@@ -22,6 +29,7 @@
         v-for="option in selected"
         :key="option"
         class="py-1"
+        data-cy="cfs-selected"
         @click="toggle(option)"
       >
         <input :checked="selected.includes(option)" type="checkbox" />
@@ -35,6 +43,7 @@
         v-for="option in unselected"
         :key="option"
         class="py-1"
+        data-cy="cfs-unselected"
         @click="toggle(option)"
       >
         <input :checked="selected.includes(option)" type="checkbox" />
@@ -46,7 +55,11 @@
       </div>
     </div>
     <div class="py-1 px-2 d-flex justify-content-end border-top">
-      <button class="btn btn-sm btn-outline-primary" @click="clear">
+      <button
+        class="btn btn-sm btn-outline-primary"
+        data-cy="cfs-clear"
+        @click="clear"
+      >
         Clear
       </button>
     </div>
@@ -95,6 +108,7 @@ export default {
     },
     /** Submit any change on close, grab latest state on open. */
     async syncValue(visible) {
+      this.filter = ""
       if (!this.options.length) await this.fetchOptions()
       const field = this.params.colDef.field
       const filter = this.datasource.getFilter(field)
